15df97b299b3cbd44d1e0f4a0df792e242767da3
Max Dekel <max@dekelnet.com>
2023-11-07 06:53:56 -0500

    something kinda works

--- gen.py
+++ gen.py
@@ -9,22 +9,24 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def genDoc(path):
     prompt = """
     You are wikiGPT. You will provide a wikipedia style info page for any topic.
-    Given a website path, you will generate a short but consise html file containing info related to the path.
-    Use a dark theme for the website. Background color: #1e1e2e.
-    Crucial: At every opportunity you will link to other relative paths of this website. Make the links #f38ba8.
+    Given a website path, you will generate an html file containing info related to the path.
+    Crucial: At every opportunity you will link to other relative paths of this website.
     Sitemap of the website so you can infer what to do:
     /index.html                             # A list of some popular topics
     /<topic>/<topic>.html                   # Info main page for the topic
     /<topic>/<subtopic>/<subtopic>.html     # Info subpage for the subtopic
-    /ai/openai/openai.html                  # Info page for the openai
     etc.
+
+    Use a dark theme for the website. Background color: #1e1e2e.
+    Use nice buttons for all the links. Make the links #f38ba8. 
+    Add other div elements and colors to make the website look nice.
 
     You will now be given a path. You will generate info for the path.
     The path you are given is: {path}
     """.format(path=path)
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.0
     )
